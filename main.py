ab95c3f4efcbdcf6fc224605e170f7e5a5d9ea41
nineoffour <james.hickey@protonmail.com>
2021-09-07 21:22:15 +0100

    Running in PyCharm Community Edition 20.2

--- main.py
+++ main.py
@@ -12,13 +12,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name,version):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name} {version}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm', '20.1.2')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
